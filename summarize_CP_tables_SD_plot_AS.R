36b8986bab90406f0e1626aed529ca1152df8b92
Theresa Swayne <theresaswayne@users.noreply.github.com>
2024-05-26 14:32:01 -0400

    updated with new files based on meeting 5/24

--- summarize_CP_tables_SD_plot_AS.R
+++ summarize_CP_tables_SD_plot_AS.R
@@ -1,6 +1,6 @@
 # summarize_CP_tables_SD_plot.R
 # R script to select mean and edge intensity data from CellProfiler object results files
-# and summarize by experiment and well (designed for Cytation imager data)
+# and summarize by file name
 # To use: Run the script.
 # Will be prompted for a file
 
@@ -20,12 +20,8 @@ objectData <- read_csv(objectFile,
 
 
 mean_counts <- objectData %>% 
-  group_by(FileName_orig) %>% 
+  group_by(Metadata_Labels) %>% 
   summarise(nCells = n(),
-            Mean_PLAMaxima = mean(Children_PLA_Maxima_Objects_Count),
-            Min_PLAMaxima = min(Children_PLA_Maxima_Objects_Count),
-            Max_PLAMaxima = max(Children_PLA_Maxima_Objects_Count),
-            SD_PLAMaxima = sd(Children_PLA_Maxima_Objects_Count),
             MeanPLAObjects = mean(Children_PLA_Objects_Count),
             MinPLAObjects = min(Children_PLA_Objects_Count),
             MaxPLAObjects = max(Children_PLA_Objects_Count),
@@ -38,21 +34,21 @@ outputFile = paste(objectName, "_summary.csv") # spaces will be inserted
 write_csv(mean_counts,file.path(parentDir, outputFile))
 
 # ---- Plot ----
-p_maxima <- ggplot(objectData,
-                 aes(x=FileName_orig,
-                     y=Children_PLA_Maxima_Objects_Count)) + 
-  geom_violin(trim=FALSE)  +
-  stat_summary(fun.data=mean_sdl,
-               geom="pointrange", color="green")
-
-outputPlot = paste(objectName, "maxima plot.pdf")
+# p_maxima <- ggplot(objectData,
+#                  aes(x=Metadata_Labels,
+#                      y=Children_PLA_Maxima_Objects_Count)) + 
+#   geom_violin(trim=FALSE)  +
+#   stat_summary(fun.data=mean_sdl,
+#                geom="pointrange", color="green")
+# 
+# outputPlot = paste(objectName, "maxima plot.pdf")
 
 # plot size can be changed according to the number of panels
-ggsave(file.path(parentDir, outputPlot))
+#ggsave(file.path(parentDir, outputPlot))
 
 
 p_obj <- ggplot(objectData,
-                   aes(x=FileName_orig,
+                   aes(x=Metadata_Labels,
                        y=Children_PLA_Objects_Count)) + 
   geom_violin(trim=FALSE)  +
   stat_summary(fun.data=mean_sdl,
